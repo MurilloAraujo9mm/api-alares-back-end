572efee8f2e90ab1bb26f2112ea532ed195e47a5
Murillo Araújo <murillo.araujo@corebiz.ag>
2023-08-03 23:11:41 -0300

    (feature): add mysql configs, docker-compose and module users data (feature/dependencies-database)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,30 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { ConfigModule } from '@nestjs/config';
+import { Order } from './users/models/order.model';
+import { Plan } from './users/models/plan.model';
+import { User } from './users/models/user.model';
 
 @Module({
-  imports: [],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'No2zCBBWBEgBBH5A8xNd',
+      database: 'api_alares_backend',
+      models: [Order, Plan, User],
+      autoLoadModels: true,
+      synchronize: true,
+    }),
+    UsersModule,
+    ConfigModule.forRoot(),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
---
+++ src/users/models/user.model.ts
@@ -0,0 +1,49 @@
+import {
+    Table,
+    Column,
+    Model,
+    DataType,
+    HasMany,
+    PrimaryKey,
+    AutoIncrement,
+} from 'sequelize-typescript';
+import { Order } from './order.model';
+
+@Table({
+    tableName: "users",
+    timestamps: false  // Desabilita a criação automática de createdAt e updatedAt
+})
+export class User extends Model {
+    @PrimaryKey
+    @AutoIncrement
+    @Column(DataType.INTEGER)
+    id_user: number;
+
+    @Column(DataType.STRING)
+    first_name: string;
+
+    @Column(DataType.STRING)
+    phone: string;
+
+    @Column(DataType.STRING)
+    email: string;
+
+    @Column({
+        type: DataType.DATE,
+        allowNull: false,
+        defaultValue: DataType.NOW,
+        field: 'created_at'
+    })
+    created_at: Date;
+
+    @Column({
+        type: DataType.DATE,
+        allowNull: false,
+        defaultValue: DataType.NOW,
+        field: 'updated_at'
+    })
+    updated_at: Date;
+
+    @HasMany(() => Order)
+    orders: Order[];
+}
